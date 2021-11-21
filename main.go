994032498d4aa5d684abf9cfa760ee2d8e06878e
Peter Morgan <peterjmorgan@gmail.com>
2021-11-21 15:26:25 -0700

    added in commandline argument parsing

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type UserThresholds struct {
 	Vul float64
@@ -20,22 +27,65 @@ func main() {
 		{"peterjmorgan/analyze-pr-action-test", 5,"py"},
 		{"peterjmorgan/analyze-pr-action-test", 10,"py"},
 	}
-	ut := UserThresholds{
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
+	_ = testData
+	//ut := UserThresholds{
+	//	0.65,
+	//	0.65,
+	//	0.65,
+	//	0.65,
+	//	0.65,
+	//}
+
+	prTypeCmd := flag.NewFlagSet("pr_type",flag.ExitOnError)
+	prTypeCmdRepo := prTypeCmd.String("r","repo","Repository (google/subcommand)")
+	prTypeCmdPR := prTypeCmd.Int("n", 1,"Number of PR/MR")
+
+	analyzeCmd := flag.NewFlagSet("analyze", flag.ExitOnError)
+	analyzeCmdRepo := analyzeCmd.String("r","repo","Repository (google/subcommand)")
+	analyzeCmdPR := analyzeCmd.Int("n", 1,"Number of PR/MR")
+	analyzeCmdThresholds := analyzeCmd.String("t", "thresholds", "Comma-separated float thresholds (VUL,MAL,LIC,ENG,AUT")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'pr_type' or 'analyze' subcommands")
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "pr_type":
+		prTypeCmd.Parse(os.Args[2:])
+		prType := PRType(*prTypeCmdRepo,*prTypeCmdPR)
+		fmt.Println(prType)
+		os.Exit(0)
+	case "analyze":
+		analyzeCmd.Parse(os.Args[2:])
+		thresholds := strings.Split(*analyzeCmdThresholds,"," )
+		someFloats := make([]float64,0)
+		for _,t := range thresholds {
+			aFloat, err := strconv.ParseFloat(t,64)
+			if err != nil {
+				fmt.Printf("couldn't parse float from %s",t)
+				os.Exit(1)
+			}
+			someFloats = append(someFloats,aFloat)
+		}
+		ut := UserThresholds{
+			someFloats[0],
+			someFloats[1],
+			someFloats[2],
+			someFloats[3],
+			someFloats[4],
+		}
+		Analyze(*analyzeCmdRepo, *analyzeCmdPR, ut)
+		os.Exit(0)
 	}
 
-	for _, td := range testData {
-		//diffText 		:= GetPRDiff(td.repo,td.prNum)
-		//prType   		:= DeterminePatchType(diffText)
-		//changes  		:= GetChanges(diffText)
-		//pkgVer   		:= GetChangedPackages(changes,prType)
-		//phylumJsonData 	:= ReadPhylumAnalysis(fmt.Sprintf("./phylum_analysis_%s.json",td.lang))
-		//phylumRiskData 	:= ParsePhylumRiskData(pkgVer, phylumJsonData, ut)
-		//_ = phylumRiskData
-		RunActionOne(td.repo, td.prNum, ut)
-	}
+	//for _, td := range testData {
+	//	//diffText 		:= GetPRDiff(td.repo,td.prNum)
+	//	//prType   		:= DeterminePatchType(diffText)
+	//	//changes  		:= GetChanges(diffText)
+	//	//pkgVer   		:= GetChangedPackages(changes,prType)
+	//	//phylumJsonData 	:= ReadPhylumAnalysis(fmt.Sprintf("./phylum_analysis_%s.json",td.lang))
+	//	//phylumRiskData 	:= ParsePhylumRiskData(pkgVer, phylumJsonData, ut)
+	//	//_ = phylumRiskData
+	//	Analyze(td.repo, td.prNum, ut)
+	//}
 }
